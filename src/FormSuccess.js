a14937ded6b6421ba2af266e6ba0223f95b8a638
matt6852 <prorokwow@mail.ru>
2021-04-14 16:22:17 +0300

    redone

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const FormSuccess = ({data}) => {
-    const { firstName, midleName } = data;
-    return (
-      <div className="modal-container isOpen">
-        <div className="modal-content">
-
+const FormSuccess = ({ data }) => {
+  const { firstName, midleName } = data;
+  return (
+    <div className="modal-container isOpen">
+      <div className="modal-content">
         <h4>
-          Поздравляю {firstName} {midleName} вы успещно зарегистрировались{" "}
+          Поздравляю {firstName} {midleName} вы успешно зарегистрировались{" "}
         </h4>
-        </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default FormSuccess
+export default FormSuccess;
